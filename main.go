8cf3958af456c4299fbe2d0843eebd81424e08be
Connor <kor.connor@gmail.com>
2023-03-16 22:10:03 +0900

    job offer 추출하여 struct 에 담기

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -13,26 +14,26 @@ type extractedJob struct {
 	id       string
 	location string
 	title    string
-	salary   string
 	summary  string
 }
 
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?keydownAccess=&searchType=search&searchword=python&panel_type=&search_optional_item=y&search_done=y&panel_count=y&preview=y&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting="
 
 func main() {
-
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < 1; i++ {
-		getPage(i)
-
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
-
+	fmt.Println(jobs)
 	fmt.Println(totalPages)
 
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page+1)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -47,14 +48,10 @@ func getPage(page int) {
 	searchCards := doc.Find(".item_recruit")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("value")
-		fmt.Println(id)
-		title := card.Find(".job_tit").Text()
-		fmt.Println(title)
-		location := card.Find(".job_condition").Text()
-		fmt.Println(location)
-
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+	return jobs
 
 }
 
@@ -88,4 +85,23 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("value")
+	title := cleanString(card.Find(".job_tit").Text())
+	location := cleanString(card.Find(".job_condition").Text())
+	summary := cleanString(card.Find(".job_sector").Text())
+	fmt.Println(id, title, location, summary)
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		summary:  summary,
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+
 }
