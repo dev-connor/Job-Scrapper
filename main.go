f3e2bfa67200fb9706cdaa36ac2a1800598a0a66
Connor <kor.connor@gmail.com>
2023-03-16 20:43:41 +0900

    임시 커밋

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-// var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
@@ -16,28 +16,6 @@ func main() {
 }
 
 func getPages() int {
-	// req, rErr := http.NewRequest("GET", baseURL, nil)
-	// checkErr(rErr)
-
-	// 프록시로 호출
-	// purl, err := url.Parse(baseURL)
-	// client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(purl)}}
-
-	// res, err := client.Do(req)
-	// checkErr(err)
-	// checkCode(res)
-
-	// defer res.Body.Close()
-
-	// doc, err := goquery.NewDocumentFromReader(res.Body)
-	// checkErr(err)
-
-	// fmt.Println(doc)
-
-	// doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-	// 	fmt.Println(s.Html())
-
-	// })
 
 	res, err := http.Get(baseURL)
 	checkErr(err)
@@ -48,7 +26,10 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	doc.Find(".pagination")
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Find("a").Length())
+
+	})
 
 	return 0
 }
