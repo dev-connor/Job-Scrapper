9d9faa40999957392bff0989d7d0fa2173922d2c
Connor <kor.connor@gmail.com>
2023-03-16 21:29:31 +0900

    title 과 location 추출하기

--- main.go
+++ main.go
@@ -4,16 +4,57 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+type extractedJob struct {
+	id       string
+	location string
+	title    string
+	salary   string
+	summary  string
+}
+
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?keydownAccess=&searchType=search&searchword=python&panel_type=&search_optional_item=y&search_done=y&panel_count=y&preview=y&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting="
 
 func main() {
 
 	totalPages := getPages()
+
+	for i := 0; i < 1; i++ {
+		getPage(i)
+
+	}
+
 	fmt.Println(totalPages)
+
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page+1)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("value")
+		fmt.Println(id)
+		title := card.Find(".job_tit").Text()
+		fmt.Println(title)
+		location := card.Find(".job_condition").Text()
+		fmt.Println(location)
+
+	})
 
 }
 
