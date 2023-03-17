10763c7f1cc97b6d78fcf28341cef6609f87a131
Connor <kor.connor@gmail.com>
2023-03-17 14:58:05 +0900

    파일삭제 추가

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"Job-Scrapper/scrapper"
-	"fmt"
+	"os"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
-func handleScrape(c echo.Context) error {
-	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	fmt.Println(term)
 
-	return nil
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
